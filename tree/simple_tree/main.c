f3cb0f9eef0619eb4bd6e369be4a052872f48011
Matheus Saldanha <matheushjsaldanha@gmail.com>
2016-09-15 23:25:29 -0300

    First working project

---
+++ tree/simple_tree/main.c
@@ -0,0 +1,64 @@
+#include "tree.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	void print(data_t *data){ printf("%d\n", *data); }
+	data_t data;
+
+	tree_t *tree = tree_create();
+	tree_print_curr(tree, print);
+
+	printf("Adding 0 to the right (should just update root)\n");
+	tree_append_data(tree, NULL, TRUE);
+	tree_print_curr(tree, print);
+
+	printf("Attempting to descend right\n");
+	if(!tree_descend(tree, TRUE)) printf("Could not descend\n");
+	else printf("Descended\n");
+
+	printf("Adding 2 to the right\n");
+	data = 2;
+	tree_append_data(tree, &data, TRUE);
+	
+	printf("Attempting to descend right\n");
+	if(!tree_descend(tree, TRUE)) printf("Could not descend\n");
+	else printf("Descended\n");
+	
+	tree_print_curr(tree, print);
+	data = 3;
+	printf("Attempting to change data\n");
+	tree_change_data(tree, &data);
+	tree_print_curr(tree, print);
+
+	printf("Adding 1 to the right\n");
+	data = 4;
+	tree_append_data(tree, &data, TRUE);
+	
+	printf("Attempting to descend right\n");
+	if(!tree_descend(tree, TRUE)) printf("Could not descend\n");
+	else printf("Descended\n");
+	tree_print_curr(tree, print);
+
+	printf("Rewinding and descending to the left\n");
+	tree_rewind(tree);
+	tree_descend(tree, FALSE);
+	tree_print_curr(tree, print);
+
+	printf("Adding 5 to the left\n");
+	data = 5;
+	tree_append_data(tree, &data, FALSE);
+	tree_print_curr(tree, print);
+	
+	printf("Attempting to descend left\n");
+	if(!tree_descend(tree, FALSE)) printf("Could not descend\n");
+	else printf("Descended\n");
+	tree_print_curr(tree, print);
+
+	if(tree_reached_leaf(tree)) printf("We are on a leaf\n");
+
+	tree_destroy(&tree);
+	tree_destroy(&tree);
+	tree_destroy(&tree);
+	tree_destroy(&tree);
+	return 0;
+}
--- tree/simple_tree/tree.h
+++ tree/simple_tree/tree.h
@@ -8,4 +8,15 @@ typedef int data_t;
 typedef enum {FALSE, TRUE} bool;
 typedef struct tree_struct tree_t;
 
+tree_t * tree_create();
+void tree_destroy(tree_t **);
+bool tree_is_empty(tree_t *);
+bool tree_reached_leaf(tree_t *);
+data_t *tree_get_data(tree_t *);
+bool tree_change_data(tree_t *, data_t *);
+bool tree_append_data(tree_t *, data_t *, bool);
+bool tree_descend(tree_t *, bool);
+bool tree_rewind(tree_t *);
+void tree_print_curr(tree_t *, void (*)(data_t *));
+
 #endif

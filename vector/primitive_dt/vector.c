18ab2053b14968982d9971a555f7decf5c549e13
Matheus Saldanha <matheushjsaldanha@gmail.com>
2016-10-08 18:43:07 -0300

    Made and benchmarked maxmin()

--- vector/primitive_dt/vector.c
+++ vector/primitive_dt/vector.c
@@ -3,6 +3,9 @@
 #include <time.h>
 #include <vector.h>
 
+#define MAX(X,Y) X > Y ? X : Y
+#define MIN(X,Y) X < Y ? X : Y
+
 data_t *vector_alloc(int size){
 	return (data_t *) malloc(sizeof(data_t) * size);
 }
@@ -36,41 +39,16 @@ void vector_maxmin(data_t *vector, int size, data_t *rmax, data_t *rmin){
 	max = 1<<(sizeof(data_t) * 8 - 1);
 	min = ~max;
 
-	for(i = 0; i < size; i++)
+	for(i = 0; i < size; i++){
 		if(vector[i] > max) max = vector[i];
-		else if(vector[i] < min) min = vector[i];
+		if(vector[i] < min) min = vector[i];
+	}
 
 	*rmax = max;
 	*rmin = min;
 }
 
-static int rmax, rmin;
-void vector_maxmin_r_op(data_t *vector, int beg, int end){
-	int mid = (end+beg)/2;
-	if(mid == beg) return;
-	if(end - beg == 1){
-		
-	}
-	vector_maxmin_r_op(vector, mid, end);
-	vector_maxmin_r_op(vector, mid-1, end);
-}
-
-void vector_maxmin_r(data_t *vector, int size, data_t *max, data_t *min){
-	rmax = 1<<(sizeof(data_t) * 8 - 1);
-	rmin = ~rmax;
-	vector_maxmin_r_op(vector, 0, size-1);
-	*max = rmax;
-	*min = rmin;
-}
-
 int main(int argc, char *argv[]){
-	data_t *vec = vector_create_random(20, 100);
-	data_t max, min;
-
-	vector_maxmin(vec, 20, &max, &min);
-	printf("%d %d\n", max, min);
-	vector_print(vec, 20);
-
-	free(vec);
+	
 	return 0;
 }

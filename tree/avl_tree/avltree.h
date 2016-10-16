e20719db47057fea5970d64471f359c26f2d75ff
Matheus Saldanha <matheushjsaldanha@gmail.com>
2016-10-16 19:34:50 -0200

    First file templates for avltree

---
+++ tree/avl_tree/avltree.h
@@ -0,0 +1,6 @@
+#ifndef _AVLTREE_H
+#define _AVLTREE_H
+
+typedef int data_t;
+
+#endif
---
+++ tree/avl_tree/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <avltree.h>
+
+int main(int argc, char *argv[]){
+	return 0;
+}

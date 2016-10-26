04a3a8f2e33c85e6d9a73077f7ce83e74f1480c9
Matheus Saldanha <matheushjsaldanha@gmail.com>
2016-10-26 19:14:58 -0200

    Some uncommited changes

--- vector/integer/src/benchmark.c
+++ vector/integer/src/benchmark.c
@@ -2,23 +2,26 @@
 #include <stdio.h>
 #include <vector.h>
 #include <time.h>
-#include <quicksort.h>
+#include <countingsort.h>
 
 int main(int argc, char *argv[]){
-	int *vec;
+	int *vec, bound;
 	unsigned int size;
 	clock_t begin, end;
-	//int *R;
+	int *R;
 
+	bound = 100;
 	for(size = 1000; size < 1<<30; size += 1000){
-		vec = vector_create_random(size, size*5);
+		vec = vector_create_random(size, bound);
 		
 		begin = clock();
 		/* Code being benchmarked starts here */
-		quicksort(vec, size);
+		//quicksort(vec, size);
+		R = countingsort(vec, size);
 		/* Code being benchmarked ends here */
 		end = clock();
 
+		free(R);
 		fprintf(stderr, "%d %lf\n", size, (end-begin)/(double) CLOCKS_PER_SEC);
 		free(vec);
 	}

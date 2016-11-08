88fdc0986a7e9fbfd39e3d8ac2220e78921f18eb
Matheus Saldanha <matheushjsaldanha@gmail.com>
2016-11-08 01:42:43 -0200

    Add 'randomized pivotatation' to quicksort

--- vector/integer/src/benchmark.c
+++ vector/integer/src/benchmark.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <vector.h>
 #include <time.h>
-#include <countingsort.h>
+#include <quicksort.h>
 
 int main(int argc, char *argv[]){
 	int *vec, bound;
 	unsigned int size;
 	clock_t begin, end;
-	int *R;
+	//int *R;
 
 	bound = 100;
 	for(size = 1000; size < 1<<30; size += 1000){
@@ -16,12 +16,10 @@ int main(int argc, char *argv[]){
 		
 		begin = clock();
 		/* Code being benchmarked starts here */
-		//quicksort(vec, size);
-		R = countingsort(vec, size);
+		quicksort(vec, size);
 		/* Code being benchmarked ends here */
 		end = clock();
 
-		free(R);
 		fprintf(stderr, "%d %lf\n", size, (end-begin)/(double) CLOCKS_PER_SEC);
 		free(vec);
 	}
--- vector/integer/src/quicksort.c
+++ vector/integer/src/quicksort.c
@@ -1,6 +1,7 @@
 #include <vector.h>
 #include <utils.h>
 
+/*
 static
 int partition(int *vector, int left, int right) {
 	int i, j;
@@ -25,4 +26,69 @@ void quicksort(data_t *vec, int size){
 	mid = partition(vec, 0, size-1);
 	quicksort(vec, mid);
 	quicksort(vec+mid+1, size-mid-1);
+}
+*/
+
+#define MEDIAN(X, Y, Z) (X<=Y?(X>=Z?X:(Z<=Y?Z:Y)):(Y>=Z?Y:(X<=Z?X:Z)))
+static
+void partition_med(int *vector, int left, int right, int *di, int *dj){
+	int mid, i, j;
+
+	mid = MEDIAN(vector[(right+left)/2], vector[left], vector[right]);
+
+	i = left;
+	j = right;
+	while(i <= j){
+		while(vector[i] < mid) i++;
+		while(vector[j] > mid) j--;
+		if(i > j) break;
+
+		swap(vector, i, j);
+		i++;
+		j--;
+	}
+	
+	*di = i;
+	*dj = j;
+}
+
+//What is guaranteed is that:
+// [left,j] inteval contains numbers <= pivot
+// [i,right] interval contains numbers >= pivot
+//there might be items in interval (j,i)
+//these items will have equal value to the pivot, so they end up in their correct places.
+static
+void partition(int *vector, int left, int right, int *di, int *dj){
+	int mid, i, j;
+
+	mid = vector[(right+left)/2];
+	i = left;
+	j = right;
+	while(i <= j){
+		while(vector[i] < mid) i++;
+		while(vector[j] > mid) j--;
+		if(i > j) break;
+
+		swap(vector, i, j);
+		i++;
+		j--;
+	}
+	
+	*di = i;
+	*dj = j;
+}
+
+void quicksort_op(data_t *vec, int left, int right){
+	int i, j;
+
+	if(right-left < 1) return;
+
+	partition_med(vec, left, right, &i, &j);
+	quicksort_op(vec, left, j);
+	quicksort_op(vec, i, right);
+}
+
+//Wrapper function
+void quicksort(data_t *vec, int size){
+	quicksort_op(vec, 0, size-1);
 }

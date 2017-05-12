7d306d267a26513cdf54713ec95834dfeb5c976a
Matheus Saldanha <matheus.saldanha@usp.br>
2017-05-11 22:40:42 -0300

    Implement union() and find(), and documentation

--- set/union_find/elf_set.c
+++ set/union_find/elf_set.c
@@ -1,20 +1,20 @@
-
+#include <stdlib.h>
 
 //Set implemented using Union-Find algorithms.
-//It's not supposed to have the intersection() operation. If it exists, though, it won't be efficient.
-typedef _ElfSet {
-	int *array;    //Vector of each element's set
+//It's not supposed to have the intersection() operation.
+typedef struct _ElfSet {
+	int *id;    //Vector of each element's set
 	int *weight;   //Vector of each element's weight (number of elements in its subset tree)
 	int size;      //Number of elements
 } ElfSet;
 
 ElfSet *elfSet_new(int size){
 	ElfSet *set = malloc(sizeof(ElfSet));
-	set->array = malloc(sizeof(int) * size);
+	set->id = malloc(sizeof(int) * size);
 	set->weight = malloc(sizeof(int) * size);
 	set->size = size;
 	for(size--; size >= 0; size--){
-		set->array[size] = size; //Each element begins in its own set.
+		set->id[size] = size; //Each element begins in its own set.
 		set->weight[size] = 1;   //Each element begins being pointer only by itself. 
 	}
 	return set;
@@ -24,7 +24,7 @@ void elfSet_destroy(ElfSet **set_p){
 	ElfSet *set;
 	if(set_p){
 		set = *set_p;
-		free(set->array);
+		free(set->id);
 		free(set->weight);
 		free(set);
 		*set_p = NULL;
@@ -32,9 +32,28 @@ void elfSet_destroy(ElfSet **set_p){
 }
 
 int elfSet_find(ElfSet *set, int elem){
-	if( set->array[elem] == elem ){
+	if( set->id[elem] == elem ){
 		return elem;
 	} else {
-		return elfSet_find(set, set->array[elem]);
+		// Strong path compression
+		set->id[elem] = elfSet_find(set, set->id[elem]);
+		return set->id[elem];
+	}
+}
+
+void elfSet_union(ElfSet *set, int e1, int e2){
+	int parent1, parent2;
+	parent1 = elfSet_find(set, e1);
+	parent2 = elfSet_find(set, e2);
+
+	// Weighted Quick Union
+	if( parent1 != parent2 ){
+		if( set->weight[parent1] > set->weight[parent2] ){
+			set->id[parent2] = parent1;
+			set->weight[parent1] += set->weight[parent2];
+		} else {
+			set->id[parent1] = parent2;
+			set->weight[parent2] += set->weight[parent1];
+		}
 	}
 }
---
+++ set/union_find/elf_set.h
@@ -0,0 +1,35 @@
+#ifndef _ELF_SET_H
+#define _ELF_SET_H
+
+typedef struct _ElfSet ElfSet;
+
+ElfSet *elfSet_new(int size);
+void elfSet_destroy(ElfSet **set_p);
+int elfSet_find(ElfSet *set, int elem);
+void elfSet_union(ElfSet *set, int e1, int e2);
+
+/* DOCUMENTATION
+ 
+typedef ElfSet
+	- Structure that represents a set that uses Union-Find algorithms.
+	- If the set holds N elements, they are mandatorily numbered from 0 to N-1.
+ 
+ElfSet *elfSet_new(int size);
+	- Returns a new set with 'size' elements.
+	- Initially, each element constitutes its own set.
+
+void elfSet_destroy(ElfSet **set_p);
+	- Deallocates memory allocated for a set.
+
+int elfSet_find(ElfSet *set, int elem);
+	- Returns the identifier of the set that contains element 'elem'.
+	- Complexity O(log(n)), but usually executed in O(1).
+
+void elfSet_union(ElfSet *set, int e1, int e2);
+	- Performs the union operation on 2 sets, each of which contain either 'e1' or 'e2'.
+	- In case the elements belong to the same set, do nothing.
+	- Complexity O(1).
+
+*/
+
+#endif

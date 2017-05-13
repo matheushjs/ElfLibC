d21eb865f8c2735706b10115e435ff0751adbde6
Matheus Saldanha <matheus.saldanha@usp.br>
2017-05-13 19:11:17 -0300

    Improved main()

--- vector/vector_int/main.c
+++ vector/vector_int/main.c
@@ -2,11 +2,25 @@
 
 #include <elf_vector.h>
 
+void test(ElfVector *vec){
+	int i;
+
+	for(i = 0; i < 100; i++)
+		elfVector_pushBack(vec, i);
+
+	elfVector_print(vec);
+	printf("\n");
+}
+
 int main(int argc, char *argv[]){
 	ElfVector *vec;
 
 	vec = elfVector_new_withValue(10, 1);
-	elfVector_print(vec);
+	test(vec);
+	elfVector_destroy(&vec);
+
+	vec = elfVector_new();
+	test(vec);
 	elfVector_destroy(&vec);
 
 	return 0;

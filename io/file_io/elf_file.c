f360ca780219332e28bd649376b75664dd9b92d9
Matheus Saldanha <matheus.saldanha@usp.br>
2017-07-23 00:26:08 -0300

    Remove old files

--- io/file_io/elf_file.c
+++ io/file_io/elf_file.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool elfFile_fileExist(const char *filename){
+//elfFile_size
+//elfFile_eof
+//CSV handling
+
+bool elfFile_exists(const char *filename){
 	FILE *fp = fopen(filename, "r");
 	if(!fp) return false;
 	fseek(fp, -1, SEEK_CUR);

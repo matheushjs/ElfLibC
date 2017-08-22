1ded75f499dde847498ada9ac0ac8333f8b054f4
Matheus Saldanha <matheus.saldanha@usp.br>
2017-08-22 18:22:34 -0300

    Add model

--- ui/choice_dialog/elf_choice_dialog.c
+++ ui/choice_dialog/elf_choice_dialog.c
@@ -6,13 +6,36 @@
 #include <elf_string.h> // Manipulation of non-utf8 strings
 
 typedef struct _ElfChoiceDialog {
+	char *header;    // Header string. Should be optional
+	char *text;      // Text string. Should be mandatory
+
 	char **options;  // Options to give the user
-	int optionCount; // Number of options
+	int optionCount; // Number of options (Maximum of 100 options)
 	
-	char *optionZero;   // User can specify the option 0 (usually "leave" or "cancel"
-	bool hasOptionZero; // Stores whether user gave option 0 or not.
+	char *optionZero;   // User can specify the option 0 (usually "leave" or "cancel")
 
 	ElfCanvas *canvas;
 	bool invallid; // Checks if canvas exist and if it's valid.
 	               // It becomes invallid if user changed the dialog since last printing.
 } ElfChoiceDialog;
+
+/*
+
+   .-------------------------.
+   |      Header             |
+   |-------------------------|
+   |    Text:                |
+   |                         |
+   |  [1] -                  |
+   |  [2] -                  |
+   |  [3] -                  |
+   |  [4] -                  |
+   |  [5] -                  |
+   |  [6] -                  |
+   |  [7] -                  |
+   | [11] -                  |  - 11 is just for illustration
+   |-------------------------|
+   |  [0] - Exit             |
+   '-------------------------'
+
+*/

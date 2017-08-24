fb36c9571ba4119d8d469e778d142634b1a69eaa
Matheus Saldanha <matheus.saldanha@usp.br>
2017-08-24 11:38:56 -0300

    Change signed-ness of strings in ElfEncodings

--- string/encodings/main.c
+++ string/encodings/main.c
@@ -9,30 +9,30 @@ const static char test_string[];
 
 void test(){
 	char u1[] = "は", u2[] = "é";
-	char lat1 = elfEncodings_toLatin1_utf8((unsigned char *) "é", NULL);
+	char lat1 = elfEncodings_toLatin1_utf8("é", NULL);
 	char buf[3] = "\0\0";
 
 	printf("utf1: %s\n", u1);
 	printf("utf2: %s\n", u2);
-	elfEncodings_toUtf8_latin1(lat1, (unsigned char *) &buf[0], (unsigned char *) &buf[1]);
+	elfEncodings_toUtf8_latin1(lat1, &buf[0], &buf[1]);
 	printf("lat1: %s\n", buf);
 
-	elfEncodings_lower_utf8((unsigned char *) u1);
-	elfEncodings_lower_utf8((unsigned char *) u2);
+	elfEncodings_lower_utf8(u1);
+	elfEncodings_lower_utf8(u2);
 	lat1 = elfEncodings_lower_latin1(lat1);
 
 	printf("utf1: %s\n", u1);
 	printf("utf2: %s\n", u2);
-	elfEncodings_toUtf8_latin1(lat1, (unsigned char *) &buf[0], (unsigned char *) &buf[1]);
+	elfEncodings_toUtf8_latin1(lat1, &buf[0], &buf[1]);
 	printf("lat1: %s\n", buf);
 
-	elfEncodings_upper_utf8((unsigned char *) u1);
-	elfEncodings_upper_utf8((unsigned char *) u2);
+	elfEncodings_upper_utf8(u1);
+	elfEncodings_upper_utf8(u2);
 	lat1 = elfEncodings_upper_latin1(lat1);
 
 	printf("utf1: %s\n", u1);
 	printf("utf2: %s\n", u2);
-	elfEncodings_toUtf8_latin1(lat1, (unsigned char *) &buf[0], (unsigned char *) &buf[1]);
+	elfEncodings_toUtf8_latin1(lat1, &buf[0], &buf[1]);
 	printf("lat1: %s\n", buf);
 
 	printf("Length %s: %d\n", u1, elfEncodings_charLength_utf8(*u1));
@@ -43,7 +43,7 @@ void test(){
 
 	int i;
 	for(i = 0; i < 5; i++){
-		u4 = (char*) elfEncodings_advanceChar_utf8((unsigned char *) u4);
+		u4 = (char*) elfEncodings_advanceChar_utf8(u4);
 		printf("Advance: %s\n", u4);
 	}
 }

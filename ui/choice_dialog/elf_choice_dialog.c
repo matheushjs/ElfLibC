531e8c51ae6cff7f43bf3c5ecdea9bc9fa67551f
Matheus Saldanha <matheus.saldanha@usp.br>
2017-08-27 16:49:54 -0300

    Write down future functions

--- ui/choice_dialog/elf_choice_dialog.c
+++ ui/choice_dialog/elf_choice_dialog.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include <elf_canvas.h>
 #include <elf_string.h> // Manipulation of non-utf8 strings
@@ -18,9 +19,9 @@ typedef struct _ElfChoiceDialog {
 
 	ElfCanvas *canvas;
 	int width; // width, in characters, of the canvas. Should be optional (but with a nice default value).
-
-	bool canvasInvallid; // Checks if canvas exist and if it's valid.
-	               // It becomes invallid if user changed the dialog since last printing.
+	bool canvasInvallid;  // Checks if canvas exist and if it's valid.
+	                      // It becomes invallid if user changed the dialog since last printing.
+	char *builtInterface; // Stores the last build interface, in case user requests it multiple times.
 } ElfChoiceDialog;
 
 // Documented in header file.
@@ -41,7 +42,6 @@ ElfChoiceDialog *elfChoiceDialog_new(){
 	elf->canvas = NULL;
 	elf->width = DEFAULT_WIDTH;
 
-	// Canvas is 
 	elf->canvasInvallid = true;
 
 	return elf;
@@ -68,6 +68,51 @@ void elfChoiceDialog_destroy(ElfChoiceDialog **elf_p){
 	}
 }
 
+/*
+ * TODO
+ */
 
+void elfChoiceDialog_setHeader(ElfChoiceDialog *elf, const char *text){
 
+}
 
+void elfChoiceDialog_setText(ElfChoiceDialog *elf, const char *text){
+
+}
+
+int elfChoiceDialog_addChoice(ElfChoiceDialog *elf, const char *text){
+
+}
+
+void elfChoiceDialog_removeChoice(ElfChoiceDialog *elf, int choiceNum){
+
+}
+
+void elfChoiceDialog_changeChoice(ElfChoiceDialog *elf, int choiceNum, const char *text){
+
+}
+
+void elfChoiceDialog_setChoiceZero(ElfChoiceDialog *elf, const char *text){
+	// Can accept null
+}
+
+void elfChoiceDialog_setWidth(ElfChoiceDialog *elf, int width){
+
+}
+
+const
+char *elfChoiceDialog_getInterface(ElfChoiceDialog *elf){
+
+}
+
+void elfChoiceDialog_fprint(ElfChoiceDialog *elf, FILE *fp){
+
+}
+
+void elfChoiceDialog_print(ElfChoiceDialog *elf){
+	elfChoiceDialog_fprint(elf, stdin);
+}
+
+int elfChoiceDialog_prompt(ElfChoiceDialog *elf){
+	// Print interface to stdin and return the number typed.
+}
